92e1b23e65ccd0e08572c46dd0188b2503baff37
06luki06 <lukas.riegler2000@gmail.com>
2025-08-30 20:59:46 +0200

    Add functionality for adding dishes to specific cats

--- Shared/DTOs/Requests/CatRequestDto.cs
+++ Shared/DTOs/Requests/CatRequestDto.cs
@@ -5,6 +5,11 @@ namespace At.luki0606.FleduSnack.Shared.DTOs.Requests
     public class CatRequestDto
     {
         [Required, MaxLength(100)]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
+
+        public CatRequestDto(string name)
+        {
+            Name = name;
+        }
     }
 }
--- Shared/DTOs/Requests/DishRequestDto.cs
+++ Shared/DTOs/Requests/DishRequestDto.cs
@@ -6,12 +6,19 @@ namespace At.luki0606.FleduSnack.Shared.DTOs.Requests
     public class DishRequestDto
     {
         [Required, MaxLength(100)]
-        public string Brand { get; set; } = string.Empty;
+        public string Brand { get; set; }
 
         [Required, MaxLength(100)]
-        public string Flavor { get; set; } = string.Empty;
+        public string Flavor { get; set; }
 
         [Required]
         public Tasting Tasting { get; set; }
+
+        public DishRequestDto(string brand, string flavor, Tasting tasting)
+        {
+            Brand = brand;
+            Flavor = flavor;
+            Tasting = tasting;
+        }
     }
 }
